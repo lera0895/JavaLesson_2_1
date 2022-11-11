286a484a9b8630cbb214db2a1805eac29c48df8c
Лера <Lera290897@gmail.com>
2022-11-11 15:33:26 +0300

    Бонусные мили

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        int price = 5000; //стоимость билета
+        int rub = 20; // рублей для одной бонусной мили
+        System.out.println (price/rub   +   " бонусных миль ");
 
-        System.out.println("Hello world!");
-        int i = 0;
-    }
-}
+                }
+                }
