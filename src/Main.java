36259b547c50a4d4d761ad847dbad09b559dd59e
Лера <Lera290897@gmail.com>
2022-11-11 15:48:58 +0300

    Форматирование кода

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int price = 5000; //стоимость билета
+        int price = 34563; //стоимость билета
         int rub = 20; // рублей для одной бонусной мили
-        System.out.println (price/rub   +   " бонусных миль ");
+        System.out.println(price / rub + " бонусных миль ");
 
-                }
-                }
+    }
+}
